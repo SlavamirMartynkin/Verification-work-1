d2b5d8e7e67c9c72695b4fc8e4f56cc50ba78825
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-06-06 07:26:26 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//        длина которых меньше, либо равна 3 символам
+
+
+string [] array = new string[5];
+string[] FillArray(string [] array)
+{
+    int Length = array.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        System.Console.Write("Введите строку: ");
+        array[index] = Console.ReadLine();
+        index++;
+    }
+    return array;
+}
+
+
+string[] NewArray(string [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++; 
+    }
+    
+    string [] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+System.Console.WriteLine($"[{string.Join(", ", FillArray(array))}]");
+System.Console.WriteLine($"[{string.Join(", ", NewArray(array))}]");
+
+
